a4e8f3d81da3eb86d1f5b7c87d72a13b4d571870
sjcomeau43543 <sjcomeau@wpi.edu>
2019-02-26 22:59:47 -0500

    working LED onboard light

---
+++ IoTDevices/Lights/led_lights/led_lights.ino
@@ -0,0 +1,36 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial EEBlue(10, 11); // RX | TX
+int ledPin = 13;
+int EEBlue_data;
+int Serial_data;
+
+void setup() {
+  Serial.begin(9600);
+  EEBlue.begin(9600);  //Default Baud for comm, it may be different for your Module. 
+  Serial.println("The bluetooth gates are open.\n Connect to HC-05 from any other bluetooth device with 1234 as pairing key!.");
+
+  pinMode(ledPin, OUTPUT);
+}
+ 
+void loop(){
+  // Feed any data from bluetooth to Terminal.
+  if (EEBlue.available()){
+    EEBlue_data = EEBlue.read();
+    Serial.write(EEBlue_data);
+
+    if (EEBlue_data == '0'){
+      EEBlue.println("Turning the LED off");
+      digitalWrite(ledPin, 0);
+    } else if (EEBlue_data == '1'){
+      EEBlue.println("Turning the LED on");
+      digitalWrite(ledPin, 1);
+    }
+  }
+ 
+  // Feed all data from terminal to bluetooth
+  if (Serial.available()){
+    Serial_data = Serial.read();
+    EEBlue.write(Serial_data);
+  }
+} 

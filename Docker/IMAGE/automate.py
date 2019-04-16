29465dae69c6d8a16ff8fa2a0f94792f48d07b65
sj <sjcomeau@wpi.edu>
2019-04-16 19:13:52 -0400

    cleaning, looking for SUCCESS

--- Docker/IMAGE/automate.py
+++ Docker/IMAGE/automate.py
@@ -7,46 +7,13 @@ def call(command, stdout=subprocess.PIPE):
   print 'Calling: ' + str(command)
   subprocess.call(command, stdout=stdout)
 
-# ummm this doesn't work sorry, but as long as you pair once, you don't need to re-pair
-def connect(MAC):
-  p = subprocess.Popen(['sudo', 'bluetoothctl'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-
-  p.stdin.write('power on\n')
-  time.sleep(1)
-  p.stdin.write('agent on\n')
-  time.sleep(1)
-  p.stdin.write('default-agent\n')
-  time.sleep(1)
-  p.stdin.write('scan on\n')
-  for line in iter(p.stdout.readline, ''):
-    print line
-    if MAC in line:
-      print line
-      break
-  p.stdin.write('trust '+MAC+'\n')
-  time.sleep(1)
-  p.stdin.write('exit\n')
-
-  #p.terminate()
-
-# unbinds the rfcomm port
-def unbind(port):
-  if os.path.exists('/dev/rfcomm'+port):
-    call(['sudo', 'rfcomm', 'release', port])
-
-# binds the rfcomm port
-def bind(MAC, port):
-  call(['sudo', 'rfcomm', 'bind', port, MAC])
-  call(['sudo', 'chmod', 'a+rwx', '/dev/rfcomm'+port])
-
 # writes a message to the rfcomm port (bluetooth)
 def write(serial_obj, message, port):
   serial_obj.write(message)
   response = read(serial_obj)
-  while 'Turning the LED ' not in response:
+  while 'SUCCESS' not in response:
     serial_obj.write(message)
     response = read(serial_obj)
-  #call(['sudo', 'echo', str(message)], open('/dev/rfcomm'+str(port), 'wb'))
 
 # reads for input on the rfcomm port
 def read(serial_obj):
@@ -56,12 +23,10 @@ def read(serial_obj):
   return result
 
 def main():
-#  to_bind = sys.argv[1]
-  to_bind = "false"
-  readwrite = sys.argv[2]
-  mac = sys.argv[3]
-  port = sys.argv[4]
-  message = sys.argv[5]
+  readwrite = sys.argv[1]
+  mac = sys.argv[2]
+  port = sys.argv[3]
+  message = sys.argv[4]
 
   print sys.argv
 
@@ -72,10 +37,6 @@ def main():
   if mac == "needed":
     print 'ERROR: You need to provide a mac address'
     exit()
-
-  if to_bind == "true":
-    unbind(port)
-    bind(mac, port)
 
   if readwrite == "read":
     serial_obj = serial.Serial('/dev/rfcomm'+port, 9600, timeout=1)
--- Docker/IMAGE/bind.py
+++
@@ -1,6 +0,0 @@
-import socket, sys
-
-print sys.argv[1], sys.argv[2]
-
-sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-sock.bind((sys.argv[1], int(sys.argv[2])))

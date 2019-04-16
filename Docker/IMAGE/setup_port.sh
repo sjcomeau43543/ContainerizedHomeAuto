69b1742996a36b0b8584a787db062d18975eeca0
sj <sjcomeau@wpi.edu>
2019-04-16 19:10:23 -0400

    script is bueno to be the first tast

--- Docker/IMAGE/setup_port.sh
+++ Docker/IMAGE/setup_port.sh
@@ -2,13 +2,7 @@
 
 echo $0 $1 $2
 
-pwd
-ls
+rfcomm release "$1"
+rfcomm bind "$1" "$2"
+chmod a+rwx "/dev/rfcomm$1"
 
-if [[ $2 != "needed" ]];
-then
- echo binding $2 to $1
- rfcomm release "$1"
- rfcomm bind "$1" "$2"
- chmod a+rwx "/dev/rfcomm$1"
-fi
